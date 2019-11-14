dea1eb89b482032850fa319e682e13a0def575c4
Gautam Gupta <gautamgupta1811@gmail.com>
2019-11-14 23:47:12 +0530

    Add files via upload

---
+++ main code.py
@@ -0,0 +1,25 @@
+import getpass
+import os
+user = getpass.getuser()
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+data = """
+:loop
+start python {}\\Battery_check.py
+Timeout /t 5 /nobreak
+goto :loop""".format(dir_path)
+
+f = open("battery.txt", "w")
+f.write(data)
+f.close()
+
+f = open("battery.txt", "rb")
+data = f.read()
+f.close()
+
+os.chdir(r"C:\Users\{}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup".format(user))
+
+file = open("battery.bat", "wb")
+file.write(data)
+file.close()
