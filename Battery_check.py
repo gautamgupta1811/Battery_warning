d8e23f454e09e0ebc0a89a4b5a0844c6b958c589
Gautam Gupta <gautamgupta1811@gmail.com>
2019-11-16 22:13:06 +0530

    Update Battery_check.py

--- Battery_check.py
+++ Battery_check.py
@@ -3,7 +3,7 @@ import wmi
 w = wmi.WMI()
 file = open("status.txt", "r+")
 data = file.read()
-data = int(data)
+data = int(data[0])
 for battery in w.query('select * from Win32_Battery'):
     a = battery.EstimatedChargeRemaining
     b = battery.BatteryStatus
